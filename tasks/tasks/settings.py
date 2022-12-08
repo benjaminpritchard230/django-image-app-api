a4abc500551b78f43023ef1582844dbd94f53343
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2022-12-08 04:02:35 +0000

    Changed number of posts returned in pagination

--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -145,5 +145,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 12,
 }
